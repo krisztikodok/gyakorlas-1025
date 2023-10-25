5888e8caafcaf238ea3f86efaf41ec7589c340cb
krisztikodok <135458893+krisztikodok@users.noreply.github.com>
2023-10-25 19:08:47 +0000

    done

--- main.py
+++ main.py
@@ -3,8 +3,13 @@ x=int(input("Adjon meg egy egész számot:"))
 print('{0} szám típusa ',type(x))
 
 a=3.22
-b=
-
+b="Lackó"
+c=False
+d=6+4j
+e=None
+tmb=[a,b,c,d,e,x] #list
+for i in tmb:
+  print(i," = ",type(i))
 
 txt="For just the small price of {money:.2f} dollars!"
 print(txt.format(money=49))
